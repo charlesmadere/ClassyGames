1fc961735261b9258496714984960890e3957734
Charles Madere <scootrnova@gmail.com>
2012-08-27 21:15:11 -0500

    more style work. nifty new action bar!

--- projects/android/classygames/src/edu/selu/android/classygames/GamesListActivity.java
+++ projects/android/classygames/src/edu/selu/android/classygames/GamesListActivity.java
@@ -3,7 +3,6 @@ package edu.selu.android.classygames;
 
 import android.os.Bundle;
 
-import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockActivity;
 
 
@@ -11,17 +10,12 @@ public class GamesListActivity extends SherlockActivity
 {
 
 
-	private ActionBar actionBar;
-
-
 	@Override
 	public void onCreate(final Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.games_list_activity);
-
-		actionBar = getSupportActionBar();
-		actionBar.setTitle(R.string.games_list_activity_title);
+		Utilities.styleActionBar(getResources(), getSupportActionBar());
 	}
 
 
---
+++ projects/android/classygames/src/edu/selu/android/classygames/Utilities.java
@@ -0,0 +1,29 @@
+package edu.selu.android.classygames;
+
+
+import android.content.res.Resources;
+import android.graphics.Shader.TileMode;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
+
+import com.actionbarsherlock.app.ActionBar;
+
+
+public final class Utilities
+{
+
+
+	public static void styleActionBar(Resources resources, ActionBar actionBar)
+	// This is a workaround for http://b.android.com/15340 from http://stackoverflow.com/a/5852198/132047
+	// This ensures that pre ice cream sandwich devices properly render our customized actionbar
+	{
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+		{
+			BitmapDrawable bg = (BitmapDrawable) resources.getDrawable(R.drawable.bg_actionbar);
+			bg.setTileModeXY(TileMode.REPEAT, TileMode.REPEAT);
+			actionBar.setBackgroundDrawable(bg);
+		}
+	}
+
+
+}
