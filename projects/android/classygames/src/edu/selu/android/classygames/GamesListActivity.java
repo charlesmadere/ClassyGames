7a9572d547749c8b76b73ded19105c90ebfba5c3
Tkidder132 <Tkidder132@gmail.com>
2012-09-04 14:57:38 -0500

    Working on game logic

--- projects/android/classygames/src/edu/selu/android/classygames/GamesListActivity.java
+++ projects/android/classygames/src/edu/selu/android/classygames/GamesListActivity.java
@@ -1,7 +1,12 @@
 package edu.selu.android.classygames;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockActivity;
 
@@ -16,7 +21,36 @@ public class GamesListActivity extends SherlockActivity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.games_list_activity);
 		Utilities.styleActionBar(getResources(), getSupportActionBar());
+	
+		Button newGame = (Button) findViewById(R.id.new_game);
+		newGame.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(final View v)
+			{
+				Context context = getApplicationContext();
+				CharSequence text = "NEW GAME!";
+				int duration = Toast.LENGTH_SHORT;
+				
+				Toast toast = Toast.makeText(context, text, duration);
+				toast.show();
+			}
+		});
+		
+		Button refresh = (Button) findViewById(R.id.refresh);
+		refresh.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(final View v)
+			{
+				Context context = getApplicationContext();
+				CharSequence text = "REFRESHENING";
+				int duration = Toast.LENGTH_SHORT;
+				
+				Toast toast = Toast.makeText(context, text, duration);
+				toast.show();
+			}
+		});
 	}
-
 
 }
