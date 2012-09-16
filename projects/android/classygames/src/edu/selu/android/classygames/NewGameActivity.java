0b787387aae366aff706b1276fc1ead985bc9ed4
Geonathan <vosophreak@gmail.com>
2012-09-16 10:45:33 -0500

    small progress
    need to setup list view for friends list

--- projects/android/classygames/src/edu/selu/android/classygames/NewGameActivity.java
+++ projects/android/classygames/src/edu/selu/android/classygames/NewGameActivity.java
@@ -1,11 +1,21 @@
 package edu.selu.android.classygames;
 
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.MenuItem;
+import com.facebook.android.AsyncFacebookRunner;
+import com.facebook.android.BaseRequestListener;
+import com.facebook.android.FacebookError;
+import com.facebook.android.Util;
 
 
 public class NewGameActivity extends SherlockActivity
@@ -20,6 +30,19 @@ public class NewGameActivity extends SherlockActivity
 		Utilities.styleActionBar(getResources(), getSupportActionBar());
 		// makes the back arrow visible
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		
+		//Get friends list
+				try 
+				{
+					AsyncFacebookRunner mAsyncRunner = new AsyncFacebookRunner(Utilities.facebook);
+					Bundle bundle = new Bundle();
+					bundle.putString("fields", "birthday");
+					mAsyncRunner.request("me/friends", bundle, new FriendListRequestListener());
+				}
+				catch(Exception e)
+				{
+					Log.e(Utilities.LOG_TAG, "FRIENDS_LIST_FAIL" + e.getMessage());
+				}
 	}
 
 	@Override
@@ -39,6 +62,50 @@ public class NewGameActivity extends SherlockActivity
 			default:
 				return super.onOptionsItemSelected(item);
 		}
+	}
+
+
+	//Friends List Adapter Defined
+	public class FriendListRequestListener extends BaseRequestListener {
+		String _error;
+			
+		public void onComplete(final String response) {
+	        _error = null;
+
+	        try {
+	            JSONObject json = Util.parseJson(response);
+	            final JSONArray friends = json.getJSONArray("data");
+	            
+	            NewGameActivity.this.runOnUiThread(new Runnable() {
+	                public void run() {
+	                    //Supposed to do stuff with array here
+	                }
+	            });
+
+	        } catch (JSONException e) {
+	            _error = "JSON Error in response";
+	        } catch (FacebookError e) {
+	            _error = "Facebook Error: " + e.getMessage();
+	        }
+
+	        if (_error != null)
+	        {
+	            NewGameActivity.this.runOnUiThread(new Runnable() {
+	                public void run() {
+	                    Toast.makeText(getApplicationContext(), "Error occurred:  " + 
+	                                    _error, Toast.LENGTH_LONG).show();
+	                }
+	            });
+	        }
+	    }
+		
+		@Override
+		public void onComplete(String response, Object state) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		
 	}
 
 }
