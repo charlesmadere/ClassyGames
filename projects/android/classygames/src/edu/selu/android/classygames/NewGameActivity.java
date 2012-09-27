147d67d0f8622f9ab11828972ca6a962dac0ad54
jarrad pinestraw <jarrad.pinestraw@selu.edu>
2012-09-26 23:35:07 -0500

    fixed crashing on new game
    bla bla

--- projects/android/classygames/src/edu/selu/android/classygames/NewGameActivity.java
+++ projects/android/classygames/src/edu/selu/android/classygames/NewGameActivity.java
@@ -143,7 +143,7 @@ public class NewGameActivity extends SherlockListActivity
 				case 2:
 					progressDialog.setProgress(i[0].intValue());
 
-					if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+					if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
 					{
 						UrlImageViewHelper.loadUrlDrawable(NewGameActivity.this, "https://graph.facebook.com/" + i[1] + "/picture?return_ssl_resources=1");
 					}
