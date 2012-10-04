5fae05a0579441d569af17d93ef5442fdec8b44e
Charles Madere <scootrnova@gmail.com>
2012-10-04 15:09:28 -0500

    preparing for creating a new branch to test checkers board stuff on

--- projects/android/classygames/src/edu/selu/android/classygames/CheckersGameActivity.java
+++ projects/android/classygames/src/edu/selu/android/classygames/CheckersGameActivity.java
@@ -1,33 +1,40 @@
 package edu.selu.android.classygames;
 
+
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 import android.widget.Toast;
 
-public class CheckersGameActivity extends Activity {
+import com.actionbarsherlock.app.SherlockActivity;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.checkers_game_activity);
-        
-  //Testing
-        
-        GridView gridview = (GridView) findViewById(R.id.gridView1);
-        gridview.setAdapter(new ImageAdapter(this));
-        
-        gridview.setOnItemClickListener(new OnItemClickListener()
-        {
 
-        	@Override
-        	public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-				
+public class CheckersGameActivity extends SherlockActivity
+{
+
+
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.checkers_game_activity);
+		Utilities.styleActionBar(getResources(), getSupportActionBar());
+
+		//Testing
+		GridView gridview = (GridView) findViewById(R.id.gridView1);
+		gridview.setAdapter(new ImageAdapter(this));
+
+		gridview.setOnItemClickListener(new OnItemClickListener()
+		{
+			@Override
+			public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+			{
 				Toast.makeText(CheckersGameActivity.this,""+ position, Toast.LENGTH_SHORT).show();
-        	}
-        	}); 		
-    	}
+			}
+		});
 	}
+
+
+}
