a4e2cc5b9cf290e852de078b832244cea63eda5c
Charles Madere <scootrnova@gmail.com>
2012-11-04 18:41:02 -0600

    serverside code abstraction in prep for lots of stuff

--- projects/server/classygamesserver/src/edu/selu/android/classygames/NewGame.java
+++ projects/server/classygamesserver/src/edu/selu/android/classygames/NewGame.java
@@ -78,109 +78,16 @@ public class NewGame extends HttpServlet
 				sqlStatement = sqlConnection.prepareStatement(sqlStatementString);
 
 				try
-				// huge hash creation algorithm magic below
 				{
-					// create a Random object. This will be used to generate some random values to use in the creation of a Game ID.
-					// We're seeding it with the epoch in milliseconds because this will 100% certainly always be a different number
-					// every single time that it's run, guaranteeing a strong seed.
-					Random random = new Random(System.currentTimeMillis());
-
-					// create a digest to use as the Game ID. We are going to be using the Utilities.MESSAGE_DIGEST_ALGORITHM as our
-					// hash generation algorithm. At the time of this writing it's SHA-256, but plenty more algorithms are available.
-					final MessageDigest messageDigest = MessageDigest.getInstance(Utilities.MESSAGE_DIGEST_ALGORITHM);
-
-					// Build the digest. As can be seen, we're using a bunch of different variables here. The more data we use here
-					// the better our digest will be.
-					messageDigest.update(user_challenged.toString().getBytes(Utilities.UTF8));
-					messageDigest.update(user_creator.toString().getBytes(Utilities.UTF8));
-					messageDigest.update(board.getBytes(Utilities.UTF8));
-					messageDigest.update(new Integer(random.nextInt()).toString().getBytes(Utilities.UTF8));
-
-					StringBuilder messageDigestBuilder = new StringBuilder(new BigInteger(messageDigest.digest()).abs().toString(Utilities.MESSAGE_DIGEST_RADIX));
-
-					for (int nibble = 0; messageDigestBuilder.length() < Utilities.MESSAGE_DIGEST_LENGTH; )
-					// we want a digest that's Utilities.MESSAGE_DIGEST_LENGTH characters in length. At the time of this writing, we
-					// are aiming for 64 characters long. Sometimes the digest algorithm will give us a bit less than that. So here
-					// we're making up for that shortcoming by continuously adding random characters to the digest until we get 64
-					{
-						do
-						// we don't want a negative number. keep generating random ints until we get one that's positive
-						{
-							// don't allow the random number we've generated to be above 15
-							nibble = random.nextInt() % 16;
-						}
-						while (nibble < 0);
-
-						switch (nibble)
-						{
-							case 0:
-								messageDigestBuilder.append('0');
-								break;
-
-							case 1:
-								messageDigestBuilder.append('1');
-								break;
-
-							case 2:
-								messageDigestBuilder.append('2');
-								break;
-
-							case 3:
-								messageDigestBuilder.append('3');
-								break;
-
-							case 4:
-								messageDigestBuilder.append('4');
-								break;
-
-							case 5:
-								messageDigestBuilder.append('5');
-								break;
-
-							case 6:
-								messageDigestBuilder.append('6');
-								break;
-
-							case 7:
-								messageDigestBuilder.append('7');
-								break;
-
-							case 8:
-								messageDigestBuilder.append('8');
-								break;
-
-							case 9:
-								messageDigestBuilder.append('9');
-								break;
-
-							case 10:
-								messageDigestBuilder.append('a');
-								break;
-
-							case 11:
-								messageDigestBuilder.append('b');
-								break;
-
-							case 12:
-								messageDigestBuilder.append('c');
-								break;
-
-							case 13:
-								messageDigestBuilder.append('d');
-								break;
-
-							case 14:
-								messageDigestBuilder.append('e');
-								break;
-
-							default:
-								messageDigestBuilder.append('f');
-								break;
-						}
-					}
+					final String digest = createDigest
+					(
+						user_challenged.toString().getBytes(Utilities.UTF8),
+						user_creator.toString().getBytes(Utilities.UTF8),
+						board.toString().getBytes(Utilities.UTF8)
+					);
 
 					// prevent SQL injection by inserting user data this way
-					sqlStatement.setString(1, messageDigestBuilder.toString());
+					sqlStatement.setString(1, digest);
 					sqlStatement.setLong(2, user_creator);
 					sqlStatement.setLong(3, user_challenged);
 					sqlStatement.setString(4, board);
@@ -210,6 +117,112 @@ public class NewGame extends HttpServlet
 				Utilities.closeSQL(sqlConnection, sqlStatement);
 			}
 		}
+	}
+
+
+	private String createDigest(final byte[] user_challenged_bytes, final byte[] user_creator_bytes, final byte[] board_bytes) throws NoSuchAlgorithmException, UnsupportedEncodingException
+	// huge hash creation algorithm magic below
+	{
+		// create a digest to use as the Game ID. We are going to be using the Utilities.MESSAGE_DIGEST_ALGORITHM as our
+		// hash generation algorithm. At the time of this writing it's SHA-256, but plenty more algorithms are available.
+		final MessageDigest digest = MessageDigest.getInstance(Utilities.MESSAGE_DIGEST_ALGORITHM);
+
+		// create a Random object. This will be used to generate some random values to use in the creation of a Game ID.
+		// We're seeding it with the epoch in milliseconds because this will 100% certainly always be a different number
+		// every single time that it's run, guaranteeing a strong seed.
+		Random random = new Random(System.currentTimeMillis());
+
+		// Build the digest. As can be seen, we're using a bunch of different variables here. The more data we use here
+		// the better our digest will be.
+		digest.update(user_challenged_bytes);
+		digest.update(user_creator_bytes);
+		digest.update(board_bytes);
+		digest.update(new Integer(random.nextInt()).toString().getBytes(Utilities.UTF8));
+
+		StringBuilder digestBuilder = new StringBuilder(new BigInteger(digest.digest()).abs().toString(Utilities.MESSAGE_DIGEST_RADIX));
+
+		for (int nibble = 0; digestBuilder.length() < Utilities.MESSAGE_DIGEST_LENGTH; )
+		// we want a digest that's Utilities.MESSAGE_DIGEST_LENGTH characters in length. At the time of this writing, we
+		// are aiming for 64 characters long. Sometimes the digest algorithm will give us a bit less than that. So here
+		// we're making up for that shortcoming by continuously adding random characters to the digest until we get 64
+		{
+			do
+			// we don't want a negative number. keep generating random ints until we get one that's positive
+			{
+				// don't allow the random number we've generated to be above 15
+				nibble = random.nextInt() % 16;
+			}
+			while (nibble < 0);
+
+			switch (nibble)
+			{
+				case 0:
+					digestBuilder.append('0');
+					break;
+
+				case 1:
+					digestBuilder.append('1');
+					break;
+
+				case 2:
+					digestBuilder.append('2');
+					break;
+
+				case 3:
+					digestBuilder.append('3');
+					break;
+
+				case 4:
+					digestBuilder.append('4');
+					break;
+
+				case 5:
+					digestBuilder.append('5');
+					break;
+
+				case 6:
+					digestBuilder.append('6');
+					break;
+
+				case 7:
+					digestBuilder.append('7');
+					break;
+
+				case 8:
+					digestBuilder.append('8');
+					break;
+
+				case 9:
+					digestBuilder.append('9');
+					break;
+
+				case 10:
+					digestBuilder.append('a');
+					break;
+
+				case 11:
+					digestBuilder.append('b');
+					break;
+
+				case 12:
+					digestBuilder.append('c');
+					break;
+
+				case 13:
+					digestBuilder.append('d');
+					break;
+
+				case 14:
+					digestBuilder.append('e');
+					break;
+
+				default:
+					digestBuilder.append('f');
+					break;
+			}
+		}
+
+		return digestBuilder.toString();
 	}
 
 
