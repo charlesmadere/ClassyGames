995d47feb88805072ab005735e3b8833482f3505
Charles Madere <scootrnova@gmail.com>
2012-11-08 00:12:02 -0600

    website style updates in preparation for presentation

--- projects/server/classygamesserver/WebContent/assets/js/main.js
+++ projects/server/classygamesserver/WebContent/assets/js/main.js
@@ -3,6 +3,7 @@ var currentBackgroundScrollHeight = 0;
 
 $(document).ready(function()
 {
+	resizeWindow();
 	scrollBackground();
 
 	setMouseEvents("div#github");
@@ -11,11 +12,54 @@ $(document).ready(function()
 });
 
 
+function resizeWindow()
+{
+	var windowHeight = $(window).height();
+	var windowWidth = $(window).width();
+
+	var height = 256;
+	var width = 512;
+
+	if (windowWidth >= 1536 && windowHeight >= 896)
+	{
+		height = 768;
+		width = 1536;
+	}
+	else if (windowWidth >= 1280 && windowHeight >= 768)
+	{
+		height = 640;
+		width = 1280;
+	}
+	else if (windowHeight >= 1024 && windowWidth >= 640)
+	{
+		height = 512;
+		width = 1024;
+	}
+	else if (windowWidth >= 768 && windowHeight >= 512)
+	{
+		height = 384;
+		width = 768;
+	}
+
+	$("div#logo").css("background-image", "url(\"assets/img/logo-" + width + ".png\")");
+	$("div#logo").css("margin-left", 0 - (width / 2));
+	$("div#logo").css("margin-top", 0 - (height * (4 / 7)));
+	$("div#logo").css("height", height);
+	$("div#logo").css("width", width);
+}
+
+
+$(window).resize(function()
+{
+	resizeWindow();
+});
+
+
 function scrollBackground()
 {
 	var height = 32;
 	var beginning = "rgba(0, 0, 0, 0) ";
-	var image = "url(assets/img/bg.png) ";
+	var image = "url(\"assets/img/bg.png\") ";
 	var middle = "repeat scroll 0px ";
 	var end = "px / auto padding-box border-box";
 
