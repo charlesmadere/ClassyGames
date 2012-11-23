5229a0b086f49f773d807106d3976847178d704c
Geonathan <vosophreak@gmail.com>
2012-11-23 16:26:28 -0600

    getting there...validations

--- projects/server/classygamesserver/src/edu/selu/android/classygames/games/checkers/Board.java
+++ projects/server/classygamesserver/src/edu/selu/android/classygames/games/checkers/Board.java
@@ -60,19 +60,193 @@ public class Board extends GenericBoard
 
 	@Override
 	public boolean checkValidity()
-	// TODO
 	{
 		
-
+		byte userCount = 0;
+		byte challengedCount = 0;
+		
+		for (byte x = 0; x < LENGTH_HORIZONTAL; ++x)
+		{
+			for (byte y = 0; y < LENGTH_VERTICAL; ++y)
+		    {
+				//Check to see if position is in a legal spot in the first place. For example:
+				//[0,0] , [1,1] , [0,2] , [5,7] are all positions that a piece should never land on.
+				if (((y % 2 == 0) && (x % 2 == 0)) || ((y % 2 != 0) && (x % 2 != 0)))
+				{
+					if (getPosition(x, y).hasPiece())
+		            {	
+						return false;
+		            }
+				}
+				
+				//Check to make sure all pieces are in their proper place
+				if (((y % 2 == 0) && (x % 2 != 0)) || ((y % 2 != 0) && (x % 2 == 0)))
+				{
+					if (y > 4)
+					{
+						//Check if piece is missing
+						if (!getPosition(x, y).hasPiece())
+			            {	
+							return false;
+			            }
+						
+						//Make sure this piece belongs to the challenged team
+						if (getPosition(x,y).getPiece().getTeam() != Piece.TEAM_CHALLENGED)
+						{
+							return false;
+						}
+						
+						if (!getPosition(x,y).getPiece().isTypeNormal())
+						{
+							return false;
+						}
+						
+						challengedCount++;
+					} 
+					
+					if (y < 2)
+					{
+						//Check if piece is missing
+						if (!getPosition(x, y).hasPiece())
+			            {	
+							return false;
+			            }
+						
+						//Make sure this piece belongs to the user team
+						if (getPosition(x,y).getPiece().getTeam() != Piece.TEAM_USER)
+						{
+							return false;
+						}
+						
+						if (!getPosition(x,y).getPiece().isTypeNormal())
+						{
+							return false;
+						}
+						
+						userCount++;
+					}
+					
+					if (y == 2)
+					{
+						if(getPosition(x,y).hasPiece())
+						{
+							//Make sure this piece belongs to the user team
+							if (getPosition(x,y).getPiece().getTeam() != Piece.TEAM_USER)
+							{
+								return false;
+							}
+							
+							if (!getPosition(x,y).getPiece().isTypeNormal())
+							{
+								return false;
+							}
+							
+							userCount++;
+						}
+					}
+				}
+				
+				//Only the first row should have a piece moved from a valid spot at first
+				if ((y == 7) && (x % 2 == 0) || (y == 1) && (x % 2 == 0) || (y == 5) && (x % 2 == 0) || (y == 6) && (x % 2 != 0) || (y == 0) && (x % 2 != 0))
+				{
+					if (!getPosition(x,y).hasPiece())
+					{
+						return false;
+					}
+				}
+			
+				//Make sure first move came from a valid piece
+				if (y == 3)
+				{
+					if (x == 0)
+					{
+						//Check if piece has moved from farthest left
+						if (getPosition(x, y).hasPiece())
+			            {	
+							if (getPosition((byte)(x+1),(byte)(y-1)).hasPiece())
+				            {
+								return false;
+				            }
+							
+							userCount++;
+			            }
+					}
+					//Check the rest of the pieces
+					else if (x % 2 == 0)
+					{
+						if (getPosition(x,y).hasPiece())
+						{
+							if (getPosition((byte)(x+1),(byte)(y-1)).hasPiece() && getPosition((byte)(x-1),(byte)(y-1)).hasPiece())
+				            {
+								return false;
+				            }
+							
+							if (!getPosition((byte)(x+1),(byte)(y-1)).hasPiece() && !getPosition((byte)(x-1),(byte)(y-1)).hasPiece())
+							{
+								return false;
+							}
+							
+							userCount++;
+						}
+					}
+				}
+				
+		    }
+		}
+		
+		if(challengedCount != maxTeamSize || userCount != maxTeamSize)
+		{
+			return false;
+		}
+		
 		return true;
 	}
 
 
 	@Override
 	public boolean checkValidity(final String boardJSONData)
-	// TODO
 	{
 		final Board boardNew = new Board(boardJSONData);
+		
+		byte userCount = 0;
+		byte challengedCount = 0;
+
+		for (byte x = 0; x < LENGTH_HORIZONTAL; ++x)
+		{
+			for (byte y = 0; y < LENGTH_VERTICAL; ++y)
+		    {
+				//Check to see if position is in a legal spot in the first place. For example:
+				//[0,0] , [1,1] , [0,2] , [5,7] are all positions that a piece should never land on.
+				if (((y % 2 == 0) && (x % 2 == 0)) || ((y % 2 != 0) && (x % 2 != 0)))
+				{
+					if (boardNew.getPosition(x, y).hasPiece())
+		            {	
+						return false;
+		            }
+				}
+				
+				//Head count for each team - may need to implement something to subtract if jump is detected
+				if (boardNew.getPosition(x, y).hasPiece())
+	            {	
+					if(boardNew.getPosition(x,y).getPiece().getTeam() == Piece.TEAM_CHALLENGED)
+					{
+						challengedCount++;
+					}
+					else
+					{
+						userCount++;
+					}
+	            }
+				
+				//TODO:Check if a piece moved from a previous position, then attempt to find piece in legal position
+				// probably best to check if enemy was in front first then if in the following board it got skipped
+				// or did the skipping ALSO be aware of pieces overlapping
+				if(boardNew.getPosition(x, y).hasPiece() && !getPosition(x,y).hasPiece())
+				{
+					//Something moved
+				}
+		    }	
+		}
 
 		return true;
 	}
@@ -116,10 +290,14 @@ public class Board extends GenericBoard
 		{
 			final Map<String, Object> piece = (Map<String, Object>) team.get(i);
 			final byte pieceType = Long.valueOf((Long) piece.get("type")).byteValue();
-			final List<Byte> unparsedCoordinate = (List<Byte>) piece.get("coordinate");
+			final byte pieceTeam = Long.valueOf((Long) piece.get("team")).byteValue();
+			final List<Long> unparsedCoordinate = (List<Long>) piece.get("coordinate");
+			
+			final Byte x = unparsedCoordinate.get(0).byteValue();
+			final Byte y = unparsedCoordinate.get(1).byteValue();
 
-			final Coordinate coordinate = new Coordinate(unparsedCoordinate.get(0), unparsedCoordinate.get(1));
-			positions[coordinate.getX()][coordinate.getY()] = new Position(coordinate, i, pieceType);
+			final Coordinate coordinate = new Coordinate(x, y);
+			positions[coordinate.getX()][coordinate.getY()] = new Position(coordinate, pieceTeam, pieceType);
 		}
 	}
 
