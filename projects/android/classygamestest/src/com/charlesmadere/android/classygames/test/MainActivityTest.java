ac67f507992364c78d00c96201d69fda2f17c07a
Charles Madere <scootrnova@gmail.com>
2013-04-27 23:05:01 -0500

    added MainActivityTest

---
+++ projects/android/classygamestest/src/com/charlesmadere/android/classygames/test/MainActivityTest.java
@@ -0,0 +1,19 @@
+package com.charlesmadere.android.classygames.test;
+
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.charlesmadere.android.classygames.MainActivity;
+
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity>
+{
+
+
+	public MainActivityTest()
+	{
+		super(MainActivity.class);
+	}
+
+
+}
